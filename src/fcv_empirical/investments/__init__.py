3182c475a5124a3c0b62157346b492709b3f02dd
Matias Iglesias <matuteiglesias@gmail.com>
2026-08-23 02:42:00 -0300

    feat: build contracted GeoGCDF commitment measurements

--- src/fcv_empirical/investments/__init__.py
+++ src/fcv_empirical/investments/__init__.py
@@ -20,6 +20,21 @@ from .geogcdf import (
     normalize_geogcdf_projects,
     register_geogcdf_snapshot,
 )
+from .geogcdf_measurements import (
+    GeoGCDFGeographyResult,
+    GeoGCDFGoldResult,
+    GeoGCDFPeriodResult,
+    assign_geogcdf_periods,
+    build_geogcdf_commitment_coverage,
+    build_geogcdf_commitment_gold,
+    build_geogcdf_commitment_measurement_contract,
+    relate_geogcdf_geography,
+)
+from .geogcdf_pipeline import (
+    GeoGCDFVerticalResult,
+    materialize_geogcdf_measurement,
+    materialize_geogcdf_vertical,
+)
 from .worldbank import (
     WorldBankExtraction,
     flatten_worldbank_record,
@@ -30,22 +45,33 @@ from .worldbank import (
 
 __all__ = [
     "AidDataExtraction",
+    "GeoGCDFGeographyResult",
+    "GeoGCDFGoldResult",
+    "GeoGCDFPeriodResult",
     "GeoGCDFSilverResult",
+    "GeoGCDFVerticalResult",
     "InvestmentMaterializationResult",
     "SilverTableInput",
     "WorldBankExtraction",
+    "assign_geogcdf_periods",
     "build_aiddata_annotation_candidates",
     "build_annotation_candidates",
+    "build_geogcdf_commitment_coverage",
+    "build_geogcdf_commitment_gold",
+    "build_geogcdf_commitment_measurement_contract",
     "build_worldbank_annotation_candidates",
     "extract_aiddata_workbook",
     "flatten_worldbank_record",
     "load_worldbank_pages",
     "materialize_aiddata_silver",
     "materialize_annotation_candidates",
+    "materialize_geogcdf_measurement",
     "materialize_geogcdf_silver",
+    "materialize_geogcdf_vertical",
     "materialize_worldbank_silver",
     "normalize_geogcdf_projects",
     "register_aiddata_snapshot",
     "register_geogcdf_snapshot",
     "register_worldbank_snapshot",
+    "relate_geogcdf_geography",
 ]
