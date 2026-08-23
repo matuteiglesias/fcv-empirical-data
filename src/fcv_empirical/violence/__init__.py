19b25f79b84ce972ac721bb8f421e7fe2cab5c0d
Matias Iglesias <matuteiglesias@gmail.com>
2026-08-22 22:17:08 -0300

    feat: attach ACLED events to shared geography and periods

--- src/fcv_empirical/violence/__init__.py
+++ src/fcv_empirical/violence/__init__.py
@@ -1,3 +1,17 @@
 from .acled_events import AcledSilverResult, normalize_acled_events, register_acled_snapshot
+from .acled_index import (
+    AcledGeographyResult,
+    AcledPeriodResult,
+    assign_acled_geography,
+    assign_acled_periods,
+)
 
-__all__ = ["AcledSilverResult", "normalize_acled_events", "register_acled_snapshot"]
+__all__ = [
+    "AcledGeographyResult",
+    "AcledPeriodResult",
+    "AcledSilverResult",
+    "assign_acled_geography",
+    "assign_acled_periods",
+    "normalize_acled_events",
+    "register_acled_snapshot",
+]
